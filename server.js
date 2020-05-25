b39e4b7e00573c9425520331cbde03ba7ceeb7bb
Fizal Sarif <fizal.sarif@gmail.com>
2020-05-25 19:25:36 -0400

    make server scan for creds every 5 minutes

--- server.js
+++ server.js
@@ -6,31 +6,46 @@ const axios = require("axios");
 const routes = require("./routes");
 const tasks = require("./tasks");
 const app = express();
+let scheduledTasks = [];
 
-axios.get(`${process.env.VAULT_HOST}/v1/webhooky/creds`, {
-  headers: {
-    "X-Vault-Token": process.env.VAULT_TOKEN
-  }
-})
-.then(res => {
-  // const secrets =  app.get("secrets"); in some route
-  app.set("secrets", res.data.data);
-  //set up cron jobs with creds
-  for (let key in tasks) {
-    const task = tasks[key];
-    console.log("Scheduled", task.name, "for", task.schedule);
-    cron.schedule(task.schedule, ()=> {
-      task.handler(res.data.data);
-    });
-  }
-})
-.catch(e => {
-  console.log("Cannot reach vault.")
-  for (let key in tasks) {
-    const task = tasks[key];
-    console.log("Scheduled", task.name, "for", task.schedule);
-    cron.schedule(task.schedule, task.handler);
-  }
+cron.schedule('*/5 * * * *', () => {
+  console.log("Scanning for new credentials");
+  axios.get(`${process.env.VAULT_HOST}/v1/webhooky/creds`, {
+    headers: {
+      "X-Vault-Token": process.env.VAULT_TOKEN
+    }
+  })
+  .then(res => {
+    //cleanup
+    console.log("Cleaning up scheduled tasks");
+    scheduledTasks.forEach(task => task.destroy());
+    scheduledTasks = [];
+    // const secrets =  app.get("secrets"); in some route
+    app.set("secrets", res.data.data);
+    //set up cron jobs with creds
+    for (let key in tasks) {
+      const task = tasks[key];
+      console.log("Scheduled", task.name, "for", task.schedule);
+      const temp = cron.schedule(task.schedule, ()=> {
+        task.handler(res.data.data);
+      });
+      scheduledTasks.push(temp);
+    }
+    console.log("Finished scanning for new credentials.");
+  })
+  .catch(e => {
+    console.log("Cannot reach vault.");
+    //cleanup
+    console.log("cleaning up scheduled tasks");
+    scheduledTasks.forEach(task => task.destroy());
+    scheduledTasks = [];
+    for (let key in tasks) {
+      const task = tasks[key];
+      console.log("Scheduled", task.name, "for", task.schedule);
+      const temp = cron.schedule(task.schedule, task.handler);
+      scheduledTasks.push(temp);
+    }
+  });
 });
 
 // parse application/x-www-form-urlencoded
