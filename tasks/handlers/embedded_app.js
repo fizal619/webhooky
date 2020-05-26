8c35b423e7210dbdd7097db68fc25f82893cb0fb
Fizal Sarif <fizal.sarif@gmail.com>
2020-05-25 23:02:46 -0400

    add heroku backup trigger and changed dockerfile

---
+++ tasks/handlers/embedded_app.js
@@ -0,0 +1,20 @@
+'use strict'
+const { exec } = require("child_process");
+
+module.exports = {
+  name: "embedded-app-backup",
+  schedule: "15 1 * * *",
+  handler: ({HEROKU_TOKEN} = process.env) => {
+    exec(`HEROKU_API_KEY=${HEROKU_TOKEN} heroku pg:backups:capture --app embedded-membership-app`, (error, stdout, stderr) => {
+      if (error) {
+        console.log(`[embedded-app-backup] error: ${error.message}`);
+        return;
+      }
+      if (stderr) {
+        console.log(`[embedded-app-backup] stderr: ${stderr}`);
+        return;
+      }
+      console.log(`[embedded-app-backup] stdout: ${stdout}`);
+    });
+  }
+}
